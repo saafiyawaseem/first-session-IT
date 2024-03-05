2b42d1c166ddaa3b98d69c0251c6135e5b5de511
saafiyawaseem <mwaseemraza26@gmail.com>
2024-03-05 04:45:23 -0800

    Exercises operator done
    All faculty are very coaperative and helpfull.
    Thanks for sir Kamran  Khan Tessory ,Daniyl Nagori ,specially Sir Zia.

--- main.js
+++ main.js
@@ -14,3 +14,62 @@ var age = "age is:17";
 var married, boolean = true;
 console.log(age);
 console.log(married);
+console.log("it course");
+console.log("operators");
+var n1 = 1;
+var n2 = 2;
+console.log(n1 + n2); //?
+var str1 = 1;
+var str2 = 2;
+console.log(str1 + str2); //?
+var n3 = 15;
+var n4 = 6;
+console.log(n3 - n4); //?
+var n5 = 25;
+var n6 = 5;
+console.log(n5 * n6); //?
+var n7 = 6;
+var n8 = 5;
+console.log(n7 / n8); //?
+var n9 = 10;
+var n10 = 7;
+console.log(n9 % n10); //?
+var num = 5;
+console.log(num); //5
+num += 5;
+console.log(num); //10
+num -= 5;
+console.log(num); //5
+console.log("Comparison operators");
+var num1 = 5;
+console.log(num1 == 5); //
+console.log(num1 === 5); //
+console.log(num1 != 5); //
+console.log(num1 > 8); //
+console.log(num1 < 8); //
+console.log(num1 >= 8); //
+console.log(num1 <= 8); //
+console.log("Logical operators");
+var n = 5;
+console.log(n >= 5 && n < 10); //
+console.log(n > 5 && n < 10); //
+console.log(n >= 5 || n < 10); //
+console.log(n > 5 || n < 10); //
+console.log(!(n < 10)); //
+console.log(!(n > 10)); //
+console.log("if Else and Else if statements-EX");
+console.log("if Examples");
+var X = "Pakistan";
+var correctAnswer = "Pakistan";
+if (X = correctAnswer) {
+    console.log("correct!");
+}
+console.log("else-Example");
+var X1 = "Pakistan";
+var correctAnswer1 = "pAKISTAN";
+if (X1 == correctAnswer1) {
+    console.log("Correct!");
+}
+else {
+    console.log("Wrong");
+}
--- main.ts
+++ main.ts
@@ -14,3 +14,86 @@ let age :string = "age is:17";
 let married,boolean = true
 console.log(age);
 console.log(married);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("it course");
+console.log("operators");
+let n1 =1;
+let n2 =2;
+console.log(n1+n2);//?
+let str1 =1;
+let str2 =2;
+console.log(str1+str2);//?
+let n3 =15;
+let n4 =6;
+console.log(n3-n4);//?
+let n5 =25;
+let n6 =5;
+console.log(n5*n6);//?
+let n7 =6;
+let n8 =5;
+console.log(n7/n8);//?
+let n9 =10;
+let n10 =7;
+console.log(n9%n10);//?
+let num =5;
+console.log(num);//5
+num +=5;
+console.log(num);//10
+num -=5;
+console.log(num)//5
+console.log("Comparison operators");
+let num1 =5;
+console.log(num1==5);//
+console.log(num1===5);//
+console.log(num1!=5);//
+console.log(num1>8);//
+console.log(num1<8);//
+console.log(num1>=8);//
+console.log(num1<=8);//
+console.log("Logical operators");
+let n =5;
+console.log(n>=5&& n<10);//
+console.log(n> 5&& n<10);//
+console.log(n>=5||n<10);//
+console.log(n>5||n<10);//
+console.log(!(n<10));//
+console.log(!(n>10));//
+console.log("if Else and Else if statements-EX");
+console.log("if Examples");
+let X ="Pakistan"
+let correctAnswer="Pakistan";
+if(X=correctAnswer){
+      console.log("correct!");
+}            
+console.log("else-Example");
+let X1="Pakistan"
+let correctAnswer1="pAKISTAN";
+if(X1==correctAnswer1){
+    console.log("Correct!");
+} else{                
+     console.log("Wrong");  
+}   
+
+
+
+
+
+
+
+
